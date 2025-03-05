1695751b43cc724258dde2b12d8113a975702751
FaItHnnN <160136566+FaItHnnN@users.noreply.github.com>
2025-03-05 12:08:20 +0800

    Update TerminalEvent.java

--- TerminalEvent.java
+++ TerminalEvent.java
@@ -1,27 +1,25 @@
 import java.util.Optional;
 
 class TerminalEvent extends Event {
-  
-
 
     TerminalEvent(double etime) {
         // You can assign a specific value or identifier for this event
         super(etime);
     }
-
+  
     @Override
     protected boolean isTerminalEvent() {
         return true;
-    }
-    
-
-    @Override
-    public String toString() {
-        return "";
     }
 
     @Override
     public Pair<Optional<Event>, Shop> next(Shop shop) {
         return new Pair<>(Optional.empty(), shop); // No further events after TerminalEvent
     }
+      
+    @Override
+    public String toString() {
+        return "";
+    }
+
 }
